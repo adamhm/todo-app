09e4f32bdb88b818e498fdb3b7af1a196b35c38b
Adam Hamar <adam.hamar@gmail.com>
2023-04-07 22:13:13 +0200

    create the todo app vite project

---
+++ src/components/App.tsx
@@ -0,0 +1,11 @@
+import styles from "./App.module.scss";
+
+function App() {
+    return (
+        <div className={styles.App}>
+            <p>Frontend Mentor Challenge | Todo App</p>
+        </div>
+    );
+}
+
+export default App;
