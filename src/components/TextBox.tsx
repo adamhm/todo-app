1ecae038244cb9d0ca3fb0edc450df189a8f74a3
Adam Hamar <adam.hamar@gmail.com>
2023-04-14 11:55:44 +0200

    add TextBox component

---
+++ src/components/TextBox.tsx
@@ -0,0 +1,11 @@
+import styles from "./TextBox.module.scss";
+
+function TextBox() {
+    return (
+        <div className={styles.TextBox}>
+            <input type="text" />
+        </div>
+    );
+}
+
+export default TextBox;
