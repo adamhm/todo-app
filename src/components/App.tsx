082cedd56df2c542b253be82cee481d6f2f2317d
Adam Hamar <adam.hamar@gmail.com>
2023-04-14 11:54:37 +0200

    feat: add App component style rules

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -2,15 +2,22 @@ import { useMemo, useState } from "react";
 import { Theme } from "../@types/theme";
 import ThemeContext from "../contexts/theme-context";
 import styles from "./App.module.scss";
+import TodoPanel from "./TodoPanel";
 
 function App() {
     const [theme, setTheme] = useState<Theme>("light");
-    const context = useMemo(() => ({ theme, setTheme }), []);
+    const toggleTheme = () =>
+        setTheme((prevTheme) => (prevTheme === "light" ? "dark" : "light"));
+    const context = useMemo(() => ({ theme, setTheme, toggleTheme }), [theme]);
 
     return (
         <ThemeContext.Provider value={context}>
-            <div className={theme === "light" ? styles.light : styles.dark}>
-                <p>Frontend Mentor Challenge | Todo App</p>
+            <div
+                className={`${styles.App} ${
+                    theme === "light" ? styles.light : styles.dark
+                }`}
+            >
+                <TodoPanel />
             </div>
         </ThemeContext.Provider>
     );
