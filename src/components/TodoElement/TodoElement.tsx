ddbef78eb9fdbdc20659958aa07a0c187f031897
Adam Hamar <adam.hamar@gmail.com>
2023-04-17 14:13:52 +0200

    add the TodoElement component

---
+++ src/components/TodoElement/TodoElement.tsx
@@ -0,0 +1,18 @@
+import styles from "./TodoElement.module.scss";
+import { Todo } from "../../@types/todo";
+import Checkbox from "../Checkbox";
+
+type Props = { todo: Todo };
+
+function TodoElement({ todo }: Props) {
+    return (
+        <li className={styles.TodoElement}>
+            <Checkbox checked={todo.completed} />
+            <p className={todo.completed ? styles.completed : styles.active}>
+                {todo.task}
+            </p>
+        </li>
+    );
+}
+
+export default TodoElement;
