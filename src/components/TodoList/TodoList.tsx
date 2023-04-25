4580d8ca5b3b2b4554aab0be165031ecc64fcf7c
Adam Hamar <adam.hamar@gmail.com>
2023-04-25 12:27:09 +0200

    fix the border of the last todo item

--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -1,23 +1,22 @@
-import { useContext } from "react";
 import { Todo } from "@typedefs/todo";
-import { TodoElement, ListFooter } from "@components/index";
-import TodoStateContext from "@contexts/todo-state";
+import { TodoElement } from "@components/index";
 import styles from "./TodoList.module.scss";
 
 type Props = { filteredList: Todo[] };
 
-function TodoList({ todoList }: Props) {
-    const todoStateContext = useContext(TodoStateContext);
-    let filteredList: Todo[];
+const MAX_VISIBLE_ITEMS = 6;
 
 function TodoList({ filteredList }: Props) {
+    const className =
+        filteredList.length >= MAX_VISIBLE_ITEMS
+            ? `${styles.TodoList} ${styles.full}`
+            : styles.TodoList;
 
     return (
-        <ul className={styles.TodoList}>
+        <ul className={className}>
             {filteredList.map((todo) => (
                 <TodoElement todo={todo} key={todo.id} />
             ))}
-            <ListFooter todos={filteredList} />
         </ul>
     );
 }
