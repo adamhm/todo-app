89c43ebc5fec104123ea588b2bac24d5c189f66d
Adam Hamar <adam.hamar@gmail.com>
2023-04-29 11:38:48 +0200

    add themes to the TodoList component

--- src/components/TodoList/TodoList.module.scss.d.ts
+++ src/components/TodoList/TodoList.module.scss.d.ts
@@ -1,5 +1,7 @@
 declare const classNames: {
     readonly TodoList: "TodoList";
     readonly full: "full";
+    readonly light: "light";
+    readonly dark: "dark";
 };
 export = classNames;
--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -1,5 +1,7 @@
 import { Todo } from "@typedefs/todo";
 import { TodoElement } from "@components/index";
+import { useContext } from "react";
+import ThemeContext from "@contexts/theme";
 import styles from "./TodoList.module.scss";
 
 type Props = { filteredList: Todo[] };
@@ -7,10 +9,12 @@ type Props = { filteredList: Todo[] };
 const MAX_VISIBLE_ITEMS = 6;
 
 function TodoList({ filteredList }: Props) {
+    const { theme } = useContext(ThemeContext);
+
     const className =
         filteredList.length >= MAX_VISIBLE_ITEMS
-            ? `${styles.TodoList} ${styles.full}`
-            : styles.TodoList;
+            ? `${styles.TodoList} ${styles.full} ${styles[theme]}`
+            : `${styles.TodoList} ${styles[theme]}`;
 
     return (
         <ul className={className}>
