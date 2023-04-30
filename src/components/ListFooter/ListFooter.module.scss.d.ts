eac6f4f7995720e5a27d10dfd6291e9000531b5d
Adam Hamar <adam.hamar@gmail.com>
2023-04-30 09:30:50 +0200

    set the themes in the ListFooter component

--- src/components/ListFooter/ListFooter.module.scss.d.ts
+++ src/components/ListFooter/ListFooter.module.scss.d.ts
@@ -1,4 +1,6 @@
 declare const classNames: {
     readonly ListFooter: "ListFooter";
+    readonly light: "light";
+    readonly dark: "dark";
 };
 export = classNames;
--- src/components/ListFooter/ListFooter.tsx
+++ src/components/ListFooter/ListFooter.tsx
@@ -2,15 +2,17 @@ import { Todo } from "@typedefs/todo";
 import { ListFilter } from "@components/index";
 import { useContext } from "react";
 import TodoStateContext from "@contexts/todo-state";
+import ThemeContext from "@contexts/theme";
 import styles from "./ListFooter.module.scss";
 
 type Props = { filteredList: Todo[] };
 
 function ListFooter({ filteredList }: Props) {
     const context = useContext(TodoStateContext);
+    const { theme } = useContext(ThemeContext);
 
     return (
-        <li className={styles.ListFooter}>
+        <li className={`${styles.ListFooter} ${styles[theme]}`}>
             <p>{`${filteredList.length} items left`}</p>
             <ListFilter />
             <button
