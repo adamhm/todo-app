c1a32b09294d6a845a88de0f6e1077bc3e2eebd1
Adam Hamar <adam.hamar@gmail.com>
2023-05-07 09:27:01 +0200

    add feat: scroll the newly added todo into view

--- src/components/TextBox/TextBox.tsx
+++ src/components/TextBox/TextBox.tsx
@@ -1,9 +1,4 @@
-import {
-    KeyboardEvent,
-    KeyboardEventHandler,
-    createRef,
-    useContext,
-} from "react";
+import { KeyboardEvent, KeyboardEventHandler, useContext, useRef } from "react";
 import { Checkbox } from "@components/index";
 import ThemeContext from "@contexts/theme";
 import styles from "./TextBox.module.scss";
@@ -12,10 +7,11 @@ type Props = { onAddTodo: (task: string) => void };
 
 function TextBox({ onAddTodo }: Props) {
     const { theme } = useContext(ThemeContext);
-    const inputRef = createRef<HTMLInputElement>();
+    const inputRef = useRef<HTMLInputElement>(null);
 
     const keyDownHandler: KeyboardEventHandler = (e: KeyboardEvent) => {
         if (e.key === "Enter" && inputRef.current) {
+            localStorage.setItem("newTodo", "true");
             onAddTodo(inputRef.current.value.trim());
         }
     };
--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -1,6 +1,6 @@
 import { Todo } from "@typedefs/todo";
 import { TodoElement } from "@components/index";
-import { useContext } from "react";
+import { useContext, useEffect, useRef } from "react";
 import ThemeContext from "@contexts/theme";
 import styles from "./TodoList.module.scss";
 
@@ -10,6 +10,16 @@ const MAX_VISIBLE_ITEMS = 6;
 
 function TodoList({ filteredList }: Props) {
     const { theme } = useContext(ThemeContext);
+    const list = useRef<HTMLUListElement>(null);
+
+    useEffect(() => {
+        const hasNewTodo = !!localStorage.getItem("newTodo");
+        localStorage.removeItem("newTodo");
+
+        if (hasNewTodo) {
+            list.current?.lastElementChild?.scrollIntoView();
+        }
+    });
 
     const className =
         filteredList.length >= MAX_VISIBLE_ITEMS
@@ -17,7 +27,7 @@ function TodoList({ filteredList }: Props) {
             : `${styles.TodoList} ${styles[theme]}`;
 
     return (
-        <ul className={className}>
+        <ul className={className} ref={list}>
             {filteredList.map((todo) => (
                 <TodoElement todo={todo} key={todo.id} />
             ))}
